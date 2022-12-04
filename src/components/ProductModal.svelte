4ffc4bbab9dd734dca495838b810693decba9b35
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-04 09:42:31 +0600

    commit

--- src/components/ProductModal.svelte
+++ src/components/ProductModal.svelte
@@ -41,3 +41,62 @@
     </div>
   </div>
 </div>
+
+<div class="h-screen flex justify-center items-center transition-all">
+  <!-- Card -->
+  <div
+    class="flex flex-col md:flex-row text-center md:text-left rounded-2xl bg-slate-800 shadow-2xl shadow-black p-6 m:p-16 m-3 md:m-0 space-y-10 md:space-y-0 md:space-x-10 ">
+    <!-- Image -->
+    <div>
+      <img
+        src="src/assets/img03.webp"
+        alt=""
+        class="mx-auto duration-200 w-60 hover:scale-105" />
+    </div>
+
+    <!-- content -->
+    <div class="flex flex-col space-y-6">
+      <div class="flex flex-col mb-4 space-y-3 text-center md:text-left">
+        <!-- Header -->
+        <div>
+          <div
+            class="inline-block text-sm bg-blue-600 py-1 px-3 rounded-full ">
+            Free shipping
+          </div>
+        </div>
+
+
+        <div class="max-w-sm text-2xl font-medium">
+          Razer Kraken Kitty Edt Gaming Headset Quartz
+        </div>
+        <h3 class="line-through">$799</h3>
+        <h1 class="text-5xl font-bold my-2">$599</h1>
+        <p class="mt-2 text-slate-400">
+          The offer is valid untill April 3 or as long as stock lasts!
+        </p>
+        <button
+          class="outline-none py-3 my-5 bg-blue-600 rounded-lg hover:bg-blue-600/90 shadow-[0_5px] shadow-blue-800 active:bg-blue-800 tracking-widest transition-all ease-in-out active:translate-y-[5px] active:shadow-none">
+          Add to cart
+        </button>
+
+        <!-- Buttons -->
+        <div class="flex items-center space-x-2 group">
+          <div
+            class="h-3 w-3 rounded-full bg-green-400 group-hover:animate-pulse" />
+          <div class="p-0 m-0 text-sm">50+ pcs in stock</div>
+        </div>
+
+        <div class="flex mt-5 ">
+          <button
+            class="py-3 mr-2 border-2 duration-500 border-slate-500 hover:border-slate-400 rounded-lg grow">
+            🛒 Add to cart
+          </button>
+          <button
+            class="py-3 ml-2 border-2 duration-500 border-slate-500 hover:border-slate-400 rounded-lg grow">
+            🤍 Add to wihslist
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
