250be45544e2b32b506fdf98722b9af49ca95be1
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-05 20:42:29 +0600

    commit

--- src/components/ImageGallery.svelte
+++ src/components/ImageGallery.svelte
@@ -54,26 +54,174 @@
         <img
           src="src/assets/imgs/1.jpg"
           alt=""
+          class="duration-200 w-72 rounded-lg" />
+        <div
+          class="backdrop-blur-sm absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 rounded-b-lg">
+          <div class="flex justify-between w-full">
+            <div class="font-normal">
+              <p class="text-sm">
+                Lorem, ipsum dolor sit amet consectetur adipisicing
+              </p>
+              <p class="text-xs">48 like 24 shares</p>
+            </div>
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="relative group">
+        <img
+          src="src/assets/imgs/2.jpg"
+          alt=""
           class="duration-200 w-72 rounded-lg hover:shadow-xl " />
         <div
           class="backdrop-blur-sm absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 rounded-b-lg">
           <div class="flex justify-between w-full">
             <div class="font-normal">
-              <p class="text-sm">Abstruct Painting</p>
-              <p class="text-xs">245 like</p>
-              <div class="flex items-center">
-                <svg
-                  class="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  ><path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
-              </div>
+              <p class="text-sm">
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <p class="text-xs">92 like 22 shares</p>
+            </div>
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="relative group">
+        <img
+          src="src/assets/imgs/3.jpg"
+          alt=""
+          class="duration-200 w-72 rounded-lg hover:shadow-xl " />
+        <div
+          class="backdrop-blur-sm absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 rounded-b-lg">
+          <div class="flex justify-between w-full">
+            <div class="font-normal">
+              <p class="text-sm">labore et dolore magna aliqua.</p>
+              <p class="text-xs">36 like 50 shares</p>
+            </div>
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="relative group">
+        <img
+          src="src/assets/imgs/4.jpg"
+          alt=""
+          class="duration-200 w-72 rounded-lg hover:shadow-xl " />
+        <div
+          class="backdrop-blur-sm absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 rounded-b-lg">
+          <div class="flex justify-between w-full">
+            <div class="font-normal">
+              <p class="text-sm">ncididunt ut labore et dolore magna aliqua.</p>
+              <p class="text-xs">55 like 3 shares</p>
+            </div>
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="relative group">
+        <img
+          src="src/assets/imgs/5.jpg"
+          alt=""
+          class="duration-200 w-72 rounded-lg hover:shadow-xl " />
+        <div
+          class="backdrop-blur-sm absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 rounded-b-lg">
+          <div class="flex justify-between w-full">
+            <div class="font-normal">
+              <p class="text-sm">
+                tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <p class="text-xs">32 like 13 shares</p>
+            </div>
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="relative group">
+        <img
+          src="src/assets/imgs/6.jpg"
+          alt=""
+          class="duration-200 w-72 rounded-lg hover:shadow-xl " />
+        <div
+          class="backdrop-blur-sm absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 rounded-b-lg">
+          <div class="flex justify-between w-full">
+            <div class="font-normal">
+              <p class="text-sm">
+                tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <p class="text-xs">5 like 1 share</p>
+            </div>
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
             </div>
           </div>
         </div>
