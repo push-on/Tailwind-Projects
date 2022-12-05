3d9135ac3ffe0e3ade9a8135f3ffc121f0fafc1c
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-05 15:26:30 +0600

    commit

---
+++ src/components/ImageGallery.svelte
@@ -0,0 +1,83 @@
+<!-- Global Container -->
+<div class=" flex justify-center items-center min-h-screen">
+  <!-- Card Container -->
+  <div
+    class="p-6 m-3 space-y-10 md:p-40 bg-slate-700 rounded-3xl shadow-lg shadow-black ">
+    <!-- menu Bar -->
+    <div
+      class="md:mb-24 flex flex-col md:flex-row justify-center items-center space-y-3 md:space-y-0 md:space-x-8 md:justify-end text-2xl">
+      <a
+        class="duration-500 decoration-transparent underline decoration-2 underline-offset-8 hover:decoration-white "
+        href="http://">Vector</a>
+      <a
+        class="duration-500 decoration-transparent underline decoration-2 underline-offset-8 hover:decoration-white "
+        href="http://">Illustrations</a>
+      <a
+        class="duration-500 decoration-transparent underline decoration-2 underline-offset-8 hover:decoration-white "
+        href="http://">Images</a>
+      <a
+        class="duration-500 decoration-transparent underline decoration-2 underline-offset-8 hover:decoration-white "
+        href="http://">Icons</a>
+    </div>
+    <div
+      class="flex flex-col md:justify-end md:space-x-5 space-y-5 md:flex-row md:space-y-0">
+      <!-- Search Container -->
+      <div
+        class="transition-colors duration-200 flex justify-between border-b-2 border-slate-500 focus-within:border-slate-300 ">
+        <input
+          type="text"
+          placeholder="Search"
+          class="ml-6 bg-transparent text-white outline-none flex-grow" />
+        <!-- Search Icon -->
+        <button>
+          <svg
+            class="w-8 text-slate-300 duration-200 hover:scale-105 hover:text-slate-50 "
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </button>
+      </div>
+      <!-- button upload-->
+      <button
+        class="bg-slate-900 py-3 px-14 text-lg rounded-lg duration-200 hover:bg-slate-800 "
+        >Upload</button>
+    </div>
+    <!-- Gallary container -->
+    <div class="grid gap-4 md:grid-cols-3">
+      <div class="relative group">
+        <img
+          src="src/assets/imgs/1.jpg"
+          alt=""
+          class="duration-200 w-72 rounded-lg hover:shadow-xl " />
+        <div
+          class="backdrop-blur-sm absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 rounded-b-lg">
+          <div class="flex justify-between w-full">
+            <div class="font-normal">
+              <p class="text-sm">Abstruct Painting</p>
+              <p class="text-xs">245 like</p>
+              <div class="flex items-center">
+                <svg
+                  class="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -1,4 +1,4 @@
-<div class="border-4 h-screen flex justify-center items-center">
+<div class="">
   <div class="">
     
   </div>
