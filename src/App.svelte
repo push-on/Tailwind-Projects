e5fc0eb46bfe6300aeb5340f443b73eed7d19859
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-13 18:15:09 +0600

    setting up tailwind projects

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,10 @@
 <script>
+  import Bookmark from './components/bookmark.svelte'
+  import Clipboard from './components/clipboard.svelte'
+  import Fylo from './components/fylo.svelte'
+  import Loopstudio from './components/loopstudio.svelte'
+  import Shorty from './components/shorty.svelte'
+  import Testimonial from './components/testimonial.svelte'
   import EmailSub from './components/EmailSub.svelte'
   import ImageGallery from './components/ImageGallery.svelte'
   import LoginModal from './components/LoginModal.svelte'
@@ -8,10 +14,16 @@
 </script>
 
 <main>
+  <!-- <Test /> -->
   <!-- <EmailSub /> -->
   <!-- <ImageGallery /> -->
   <!-- <LoginModal /> -->
   <!-- <PriceingCards/> -->
   <!-- <ProductModal /> -->
-  <!-- <Test /> -->
+  <!-- <Clipboard /> -->
+  <!-- <Bookmark /> -->
+  <!-- <Fylo /> -->
+  <!-- <Loopstudio /> -->
+  <!-- <Shorty /> -->
+  <!-- <Testimonial /> -->
 </main>
