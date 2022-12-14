ef30cc43fa1febd051a6a5d54fdac8bbcad54cba
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-14 18:45:14 +0600

    starter project

--- src/components/clipboard.svelte
+++ src/components/clipboard.svelte
@@ -1,5 +1,7 @@
-<div class="">
-  <div class="text-3xl underline underline-offset-8 ">
-    Hello world
+
+<!-- Hero Section -->
+<section>
+  <div class="max-w-6xl mx-auto text-center mb-40 px-10 pt-16">
+    <img src="/src/assets/clip/logo.svg" alt="" class="mx-auto my-6">
   </div>
-</div>
+</section>
